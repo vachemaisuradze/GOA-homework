2f8d0d8c6e6cccdb9b6164a19f293529a24f6851
vachemaisuradze <vachemaisuradze408@gmail.com>
2024-06-22 11:51:04 +0200

    fort commit

---
+++ level 2/homework/Lesson6.py
@@ -0,0 +1,31 @@
+num1=11 
+num2=14 
+num3=num1 + num2
+num4=num1 - num2
+
+
+
+print(10+13)
+print(7-3)
+print(5/4)
+print(2*9)
+
+print(20+18)
+print(15-19)
+print(17/21)
+print(21*16)
+ 
+print(26+24)
+print(21-28)
+print(27/29)
+print(29*23)
+
+print(31+30)
+print(30-35)
+print(30/38)
+print(37*34)
+
+print(26+30)
+print(37-40)
+print(25/45)
+print(47*50)
