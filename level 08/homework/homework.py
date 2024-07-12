d83d6735a8b56a168e382f3efa76603eb2cf7426
vachemaisuradze <vachemaisuradze408@gmail.com>
2024-07-12 16:07:35 +0200

    commit

---
+++ level 08/homework/homework.py
@@ -0,0 +1,16 @@
+lemur=10
+lemur_mango=5
+print(lemur-lemur_mango)
+# y = 4
+# print(x+y)
+# 4 a=5
+# b=2
+# print(a/b)
+# 5x=5
+# y=10
+# z=8
+# print(z)
+# 6age =25
+# print(age + 5)
+# 2 print("Hello,world!")
+# 3 x=5
